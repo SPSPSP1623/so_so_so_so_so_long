bf6e18e3a6031a2122aeadb1d6d691c85fd70cbb
Simon Peladeau <speladea@made-f0Ar11s8.clusters.42paris.fr>
2024-11-09 20:44:27 +0100

    so_so_so_so_long

---
+++ src/end.c
@@ -0,0 +1,75 @@
+#include "so_long.h"
+
+void	ft_free_map(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	while (i < map->h)
+	{
+		free(map->map[i]);
+		map->map[i] = NULL;
+		i++;
+	}
+	free(map->map);
+	map->map = NULL;
+}
+
+void	ft_free_tiles(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	while (i < map->h)
+	{
+		free(map->t[i]);
+		map->t[i] = NULL;
+		i++;
+	}
+	free(map->t);
+	map->t = NULL;
+}
+
+void	ft_destroy_image(t_core *core)
+{
+	if (core->data->twall.img)
+		mlx_destroy_image(core->mlx->ptr, core->data->twall.img);
+	if (core->data->ttile.img)
+		mlx_destroy_image(core->mlx->ptr, core->data->ttile.img);
+	if (core->data->tp_u.img)
+		mlx_destroy_image(core->mlx->ptr, core->data->tp_u.img);
+	if (core->data->tp_d.img)
+		mlx_destroy_image(core->mlx->ptr, core->data->tp_d.img);
+	if (core->data->tp_l.img)
+		mlx_destroy_image(core->mlx->ptr, core->data->tp_l.img);
+	if (core->data->tp_r.img)
+		mlx_destroy_image(core->mlx->ptr, core->data->tp_r.img);
+	if (core->data->tcol.img)
+		mlx_destroy_image(core->mlx->ptr, core->data->tcol.img);
+	if (core->data->texit.img)
+		mlx_destroy_image(core->mlx->ptr, core->data->texit.img);
+	if (core->mlx->img)
+		mlx_destroy_image(core->mlx->ptr, core->mlx->img);
+	if (core->mlx->win)
+		mlx_destroy_window(core->mlx->ptr, core->mlx->win);
+}
+
+int	ft_close_window(t_core *core)
+{
+	ft_free_map(core->map);
+	ft_free_tiles(core->map);
+	ft_destroy_image(core);
+	mlx_destroy_display(core->mlx->ptr);
+	mlx_loop_end(core->mlx->ptr);
+	free(core->mlx->ptr);
+	exit(EXIT_SUCCESS);
+	return (0);
+}
+
+void	ft_fail_before_mlx(t_core *core, char *error)
+{
+	ft_free_map(core->map);
+	ft_free_tiles(core->map);
+	w_error(error);
+	exit(EXIT_SUCCESS);
+}
---
+++ src/parsing.c
@@ -0,0 +1,80 @@
+#include "so_long.h"
+
+void	ft_map_parsing(t_data *data, t_map *map)
+{
+	int		fd;
+	char	*map_line;
+
+	fd = open(data->map_path, O_RDONLY);
+	if (fd < 0)
+		w_error_exit("Error: fd < 0\n");
+	map_line = get_next_line(fd);
+	while (map_line && map_line[0] == '\n')
+	{
+		free(map_line);
+		map_line = get_next_line(fd);
+	}
+	while (map_line)
+	{
+		map->h++;
+		if (map->h == 1)
+			map->w = ft_map_line_length(map_line);
+		if (ft_map_line_length(map_line) != map->w)
+			map->len = -1;
+		free(map_line);
+		map_line = get_next_line(fd);
+	}
+	close(fd);
+	if (map->h == 0 || map->w == 0 || map->len != 1)
+		w_error_exit("Error: map is not rectangular.\n");
+}
+
+void	ft_alloc(t_map *map)
+{
+	map->map[map->y_ax] = malloc((map->w) * sizeof(char));
+	map->t[map->y_ax] = malloc((map->w) * sizeof(t_tile));
+}
+
+void	ft_fill(t_map *map, char *line)
+{
+	map->map[map->y_ax][map->x_ax] = line[map->x_ax];
+	map->t[map->y_ax][map->x_ax].type = line[map->x_ax];
+	map->t[map->y_ax][map->x_ax].seen = 0;
+}
+
+void	ft_flood_fill(t_data *data, t_map *map)
+{
+	char	*line;
+
+	line = get_next_line(map->fd);
+	while (line && line[0] == '\n')
+	{
+		free(line);
+		line = get_next_line(map->fd);
+	}
+	while (line)
+	{
+		ft_alloc(map);
+		while (map->x_ax < map->w)
+		{
+			ft_fill(map, line);
+			ft_data_check(data, map, line[map->x_ax]);
+			map->x_ax++;
+		}
+		map->x_ax = 0;
+		map->y_ax++;
+		free(line);
+		line = get_next_line(map->fd);
+	}
+	close(map->fd);
+}
+
+void	ft_parser(t_data *data, t_map *map, t_core *core)
+{
+	ft_map_parsing(data, map);
+	init_map(data, map, core);
+	ft_flood_fill(data, map);
+	ft_wall_checker(core, map);
+	ft_checking_checker(core, data);
+	ft_init_check(core, data->pl_p.y, data->pl_p.x);
+}
